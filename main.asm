e3c890541faecd096b9326256a2341e063e9aaad
nalvarado15 <131101011+nalvarado15@users.noreply.github.com>
2023-05-03 13:58:29 -0700

    Update main.asm

--- main.asm
+++ main.asm
@@ -9,10 +9,11 @@ lower_bound: .word 1 #minimum value of a card
 upper_bound: .word 11 #maximum value of card
 player_array: .space 100 #space to put user card/values
 computer_array: .space 100 #space to put the computers card/values
-player_card_read: .asciiz "The player's values: "
-computer_card_read: .asciiz "The computer's values: "
-
-exit_output: .asciiz "Thanks for playing our game!"
+player_card_read: .asciiz "\nThe player's values: "
+computer_card_read: .asciiz "\nThe computer's values: "
+player_rolled: .asciiz "\nPlayer drew: "
+comp_rolled: .asciiz "\nComputer drew: "
+exit_output: .asciiz "\nThanks for playing our game!"
 
 
 .text 
@@ -35,14 +36,35 @@ main:
 	bgt $t0, 1, EXIT_GAME
 	blt $t0, 2, EXIT_GAME
 
-
 	before_MAIN_LOOP:
 	la $s1, player_array  #$s1 will be a global variable holding the address of the player_array
 	la $s2, computer_array  #$s2 will be a global variable holding the address of the computer_array
+	li $t3, 0 # $t3 will be our counter for player_array
+	li $t4, 0 # $t4 will be our counter for computer_array
 	#MAIN GAME LOOP
 	MAIN_LOOP:
 
 		#CREATE PLAYER HAND
+		li $v0, 4
+		la $a0, player_rolled
+		syscall
+		jal ROLL_CARD
+		# Store rolled number into player array
+		addi $t3, $t3, 1
+		sw $t1, ($s1)
+		
+		
+		#CREATE COMPUTER HAND
+		li $v0, 4
+		la $a0, comp_rolled
+		syscall
+		jal ROLL_CARD
+		# Store rolled number into comp array
+		addi $t4, $t4, 1
+		sw $t1, ($s2)
+		b CHECK_VALUES
+		
+		ROLL_CARD:
 		la $t1, lower_bound #load the address of lower_bound
 		lw $t1, ($t1)	    #load the value at address lower_bound
 		la $t2, upper_bound #load the address of the upper_bound
@@ -58,23 +80,53 @@ main:
 		addiu $sp, $sp, 16
 		
 		#Just to see what gets printed out, the rand_int() function works as intended 
-		li $v0, 1
-		la $a0, player_array($zero)
-		lw $a0, ($a0)
-		syscall
-
-
+		#li $v0, 1
+		#la $a0, player_array($zero)
+		#lw $a0, ($a0)
+		#syscall
 		
-
-
-		#CREATE COMPUTER HAND
-
+		li $v0, 1
+		move $a0, $t1
+		syscall
+		jr $ra
 
 
 		#CHECK VALUES
-
-
+		CHECK_VALUES:
+			DISPLAY_CARDS:
+				PLAYER_DISPLAY:
+				la $a0, player_card_read
+				li $v0, 4
+				syscall
+				#We need a loop that takes in the array + array length, then loops to display the cards of the player / comp
+				addiu $sp, $sp -12
+				sw $t3, 0($sp)
+				sw $s1, 4($sp)
+				jal display_cards
+				lw $ra, 8($sp)
+				addiu $sp, $sp, 12
+			
+				COMP_DISPLAY:
+				la $a0, computer_card_read
+				li $v0, 4
+				syscall
+				#We need a loop that takes in the array + array length, then loops to display the cards of the player / comp
+				addiu $sp, $sp -12
+				sw $t4, 0($sp)
+				sw $s2, 4($sp)
+				jal display_cards
+				lw $ra, 8($sp)
+				addiu $sp, $sp, 12
+			
 		#CHECK GAME_STATE {WIN/LOSS}
+		# We need a loop to go through every number in each array, add it all up, then display total.
+		# We then need to check if this total goes over 21.
+			# If value is 21, end game with win depending on who was checked
+			# If value is less than 21, ask player if they wish to draw another (y/n)
+			# If value is over 21, end game with win depeneding on who was checked
+			# If computer value is less than a number [Like 16], computer will draw a card afterwards
+				# Otherwise, computer will no longer draw
+			# IF both computer and player choose not to draw, compare totals, end game with win depending on larger value 
 
 
 
