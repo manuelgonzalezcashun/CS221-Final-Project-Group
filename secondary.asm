3004a9f784072c9101f831d8106fab42f20aad1b
Ellmaer Ranjber <98365394+mjollnir03@users.noreply.github.com>
2023-05-05 13:14:17 -0700

    Update secondary.asm

--- secondary.asm
+++ secondary.asm
@@ -1,7 +1,6 @@
 #Ellmaer, Alex, Nik, Manny
 .data
-delimiter: .asciiz ", "
-new_line: .asciiz "\n"
+delimiter: .asciiz " " #delimter is just a space
 
 
 
@@ -10,11 +9,37 @@ new_line: .asciiz "\n"
 .text
 .globl append_to_array
 append_to_array: #APPEND RANDOM VALUE TO ARRAY OF TYPE PASSED (for either user or computer)
-    #lw $t1, 0($sp) #value to append
-    #lw $t2, 4($sp) #address of where to append 
-    
-    #sw $t1, 0($t2) #save the new value to the array of the passed address
-    #jr $ra
+    lw $t1, 0($sp) #value to append
+    lw $t2, 4($sp) #address of where to append 
+    loop:
+        lw $t3, 0($t2)
+        beqz $t3, yes_append
+        addi $t2, $t2, 4
+        b loop
+    yes_append:
+    sw $t1, 0($t2) #save the new value to the array of the passed address
+    jr $ra
+
+
+.globl display_array
+display_array:
+    lw $t1, 0($sp) #address of array passed
+    loop1:
+        lw $t2, 0($t1)
+        beqz $t2, done
+        li $v0, 1
+        move $a0, $t2
+        syscall
+        li $v0, 4
+        la $a0, delimiter
+        syscall
+        addi $t1, $t1, 4
+        b loop1
+
+    done:
+    jr $ra
+
+
 
 
 
@@ -32,32 +57,10 @@ random_int: #GENERATE RANDOM VALUE for
     lw $t1, 0($sp) #lower bound aka arg 1
     lw $t2, 4($sp) #upper bound aka arg 2
     li $v0, 42     #generate value between certain range
-    move $a0, $t1
+    #move $a0, $t1
     move $a1, $t2
     syscall
     addi $a0, $a0, 1
     sw $a0, 12($sp)#where the return value will be stored in
     jr $ra
 
-.globl display_cards
-display_cards:
-	lw $t1, 0($sp) #Length of array
-	lw $t2, 4($sp) #Array of either comp or player
-	move $t0, $t1
-	display_loop:
-	blez $t0, finish
-	lw $a0, ($t2)
-	li $v0, 1
-	syscall
-	ble $t0, 1, deliminter_exit
-	la $a0, delimiter
-	li $v0, 4
-	syscall
-	deliminter_exit:
-		# Step 5: Count down the loop using addi (Add integer). Count up the array address by 4 bytes to change index of array
-		addi $t0, $t0, -1
-		addi $t2, $t2, 4
-		# Restart the loop
-		b display_loop
-	finish:
-		jr $ra
