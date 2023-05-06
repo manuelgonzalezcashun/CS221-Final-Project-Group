0c3e4c6ce48d3944db90d6a0393e475a2aa93584
Ellmaer Ranjber <98365394+mjollnir03@users.noreply.github.com>
2023-05-05 22:31:45 -0700

    Update secondary.asm

--- secondary.asm
+++ secondary.asm
@@ -3,29 +3,36 @@
 delimiter: .asciiz " " #delimter is just a space
 
 
-
-
-
 .text
-.globl append_to_array
-append_to_array: #APPEND RANDOM VALUE TO ARRAY OF TYPE PASSED (for either user or computer)
-    lw $t1, 0($sp) #value to append
-    lw $t2, 4($sp) #address of where to append 
-    loop:
-        lw $t3, 0($t2)
-        beqz $t3, yes_append
-        addi $t2, $t2, 4
-        b loop
-    yes_append:
-    sw $t1, 0($t2) #save the new value to the array of the passed address
-    jr $ra
+.globl big_chungus
+big_chungus: #only parameter will be the addrress of the array we are using
+    lw $t9, 0($sp) #load the memory address from the stack pointer
+    lw $t8, 4($sp) #The values
+    lw $t7, 8($sp) #The totals
+    random_int1:
+        li $v0, 42     
+        li $a1, 11
+        syscall
+        addi $a0, $a0, 1
+        move $t1, $a0
 
-
-.globl display_array
-display_array:
-    lw $t1, 0($sp) #address of array passed
+    append_to_array1: 
+        loop0:
+            lw $t3, 0($t9)
+            beqz $t3, yes_append
+            addi $t9, $t9, 4
+            b loop0
+        yes_append:
+        sw $t1, 0($t9) #save the new value to the array of the passed address
+        lw $t9, 0($sp) #reset the memory address
+    
+    display_arra1y:
+    li $v0, 4
+    move $a0, $t8 #display "x values:"
+    syscall
+    
     loop1:
-        lw $t2, 0($t1)
+        lw $t2, 0($t9)
         beqz $t2, done
         li $v0, 1
         move $a0, $t2
@@ -33,45 +40,30 @@ display_array:
         li $v0, 4
         la $a0, delimiter
         syscall
-        addi $t1, $t1, 4
+        addi $t9, $t9, 4
         b loop1
 
-    done:
-    jr $ra
+        done:
 
-
-
-
-
-.globl get_totals
-get_totals: 
-	lw $t1, 0($sp) #address of array of either player of computer
-	li $t3, 0      #$t3 will hold the total value of the array
-	loop4:
-		lw $t2, 0($t1)
-		beqz $t2, done4
-		add $t3, $t3, $t2
-		addi $t1, $t1 ,4
-		b loop4
+    get_totals1: 
+        lw $t9, 0($sp) #address of array of either player of computer
+        li $t3, 0      #$t3 will hold the total value of the array
+        
+        li $v0, 4
+        move $a0, $t7
+        syscall
+        loop4:
+            lw $t2, 0($t9)
+            beqz $t2, done4
+            add $t3, $t3, $t2
+            addi $t9, $t9 ,4
+            b loop4
 		
-	done4:
-	sw $t3, 8($sp)
-    jr $ra
-
-
-
-
-
-
-.globl random_int
-random_int: #GENERATE RANDOM VALUE for
-    lw $t1, 0($sp) #lower bound aka arg 1
-    lw $t2, 4($sp) #upper bound aka arg 2
-    li $v0, 42     #generate value between certain range
-    #move $a0, $t1
-    move $a1, $t2
-    syscall
-    addi $a0, $a0, 1
-    sw $a0, 12($sp)#where the return value will be stored in
-    jr $ra
+        done4: #print the total and also save to return address
+            li $v0, 1
+            move $a0, $t3
+            syscall
+            sw $t3, 16($sp)  # 4($sp) is the return value, aka the total
+            
+            jr $ra
 
