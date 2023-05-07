0e9ea102b26e07bdb1e75e4d63b0d04e9a64da39
Ellmaer Ranjber <98365394+mjollnir03@users.noreply.github.com>
2023-05-06 20:49:20 -0700

    Update secondary.asm

--- secondary.asm
+++ secondary.asm
@@ -5,34 +5,82 @@ delimiter: .asciiz " " #delimter is just a space
 
 .text
 .globl big_chungus
-big_chungus: #only parameter will be the addrress of the array we are using
+big_chungus: 
     lw $t9, 0($sp) #load the memory address from the stack pointer
-    lw $t8, 4($sp) #The values
-    lw $t7, 8($sp) #The totals
-    random_int1:
-        li $v0, 42     
-        li $a1, 11
-        syscall
-        addi $a0, $a0, 1
-        move $t1, $a0
+    lw $t8, 4($sp) #The values (string)
+    lw $t7, 8($sp) #The totals (string)
 
-    append_to_array1: 
-        loop0:
-            lw $t3, 0($t9)
-            beqz $t3, yes_append
-            addi $t9, $t9, 4
-            b loop0
-        yes_append:
-        sw $t1, 0($t9) #save the new value to the array of the passed address
+    #call the random_int()
+        addiu $sp, $sp, -12
+        sw $ra, 0($sp)
+        jal random_int
+        lw $t1, 4($sp)
+        lw $ra, 0($sp)
+        addiu $sp, $sp, 12
+
+    #call the append_to_array()
+        addiu $sp, $sp, -12
+        sw $t1, 0($sp)
+        sw $t9, 4($sp)
+        sw $ra, 8($sp)
+        jal append_to_array
+        lw $ra, 8($sp)
+        addiu $sp, $sp, 12
         lw $t9, 0($sp) #reset the memory address
     
-    display_arra1y:
+    #call the display_array()
+        addiu $sp, $sp, -12
+        sw $t9, 0($sp)
+        sw $t8, 4($sp)
+        sw $ra, 8($sp)
+        jal display_array
+        lw $ra, 8($sp)
+        addiu $sp, $sp, 12
+        lw $t9, 0($sp) #reset the memory address
+
+    #call the get_totals()
+        addiu $sp, $sp, -16
+        sw $t9, 0($sp)
+        sw $t7, 4($sp)
+        sw $ra, 8($sp)
+        jal get_totals
+        lw $ra, 8($sp)
+        lw $t3, 12($sp)
+        addiu $sp, $sp, 16
+
+        sw $t3, 16($sp)
+
+
+    jr $ra
+
+
+
+.globl append_to_array
+append_to_array: #APPEND RANDOM VALUE TO ARRAY OF TYPE PASSED (for either user or computer)
+    lw $t1, 0($sp) #value to append
+    lw $t2, 4($sp) #address of where to append 
+    loop:
+        lw $t3, 0($t2)
+        beqz $t3, yes_append
+        addi $t2, $t2, 4
+        b loop
+    yes_append:
+    sw $t1, 0($t2) #save the new value to the array of the passed address
+    jr $ra
+
+
+.globl display_array
+display_array:
+    lw $t1, 0($sp) #address of array passed
+    lw $t3, 4($sp) #display "x values:"
+
+    #display to I/O
     li $v0, 4
-    move $a0, $t8 #display "x values:"
+    move $a0, $t3 #display "x values:"
     syscall
-    
+
     loop1:
-        lw $t2, 0($t9)
+        lw $t2, 0($t1)
         beqz $t2, done
         li $v0, 1
         move $a0, $t2
@@ -40,99 +88,49 @@ big_chungus: #only parameter will be the addrress of the array we are using
         li $v0, 4
         la $a0, delimiter
         syscall
-        addi $t9, $t9, 4
+        addi $t1, $t1, 4
         b loop1
 
-        done:
+    done:
+    jr $ra
 
-    get_totals1: 
-        lw $t9, 0($sp) #address of array of either player of computer
-        li $t3, 0      #$t3 will hold the total value of the array
-        
-        li $v0, 4
-        move $a0, $t7
+
+
+.globl get_totals
+get_totals: 
+    lw $t1, 0($sp) #address of array of either player of computer
+    lw $t4, 4($sp) #prints "x totals: "
+    li $t3, 0      #$t3 will hold the total value of the array
+    li $v0, 4
+    move $a0, $t4
+    syscall
+    loop4:
+        lw $t2, 0($t1)
+        beqz $t2, done4
+        add $t3, $t3, $t2
+        addi $t1, $t1 ,4
+        b loop4
+
+    done4:#print out the totals
+        li $v0, 1
+        move $a0, $t3
         syscall
-        loop4:
-            lw $t2, 0($t9)
-            beqz $t2, done4
-            add $t3, $t3, $t2
-            addi $t9, $t9 ,4
-            b loop4
-		
-        done4: #print the total and also save to return address
-            li $v0, 1
-            move $a0, $t3
-            syscall
-            sw $t3, 16($sp)  # 4($sp) is the return value, aka the total
-            
-            jr $ra
+        sw $t3, 12($sp)
+        jr $ra
 
-#.globl append_to_array
-#append_to_array: #APPEND RANDOM VALUE TO ARRAY OF TYPE PASSED (for either user or computer)
-#    lw $t1, 0($sp) #value to append
-#   lw $t2, 4($sp) #address of where to append 
-#    loop:
-#        lw $t3, 0($t2)
-#        beqz $t3, yes_append
-#        addi $t2, $t2, 4
-#        b loop
-#    yes_append:
-#    sw $t1, 0($t2) #save the new value to the array of the passed address
-#    jr $ra
-#
-#
-#.globl display_array
-#display_array:
-#    lw $t1, 0($sp) #address of array passed
-#    loop1:
-#        lw $t2, 0($t1)
-#        beqz $t2, done
-#        li $v0, 1
-#        move $a0, $t2
-#        syscall
-#        li $v0, 4
-#        la $a0, delimiter
-#        syscall
-#        addi $t1, $t1, 4
-#        b loop1
-#
-#    done:
-#    jr $ra
-#
-#
-#
-#
-#
-#.globl get_totals
-#get_totals: 
-#    lw $t1, 0($sp) #address of array of either player of computer
-#    li $t3, 0      #$t3 will hold the total value of the array
-#    loop4:
-#        lw $t2, 0($t1)
-#        beqz $t2, done4
-#        add $t3, $t3, $t2
-#        addi $t1, $t1 ,4
-#        b loop4
-#
-#    done4:
-#    sw $t3, 8($sp)
-#    jr $ra
-#
-#
-#.globl consider_winner
-#consider_winner:
-#
-#
-#    jr $ra
-#
-#
-#
-#.globl random_int
-#random_int: #GENERATE RANDOM VALUE for
-#    lw $t2, 4($sp) #upper bound aka arg 2
-#    li $v0, 42     #generate value between certain range
-#    move $a1, $t2
-#    syscall
-#    addi $a0, $a0, 1
-#    sw $a0, 12($sp)#where the return value will be stored in
-#    jr $ra
+
+.globl random_int
+random_int: #GENERATE RANDOM VALUE between 1 - 11
+    li $v0, 42     
+    li $a1, 11
+    syscall
+    addi $a0, $a0, 1
+    sw $a0, 4($sp)#where the return value will be stored in
+    jr $ra
+
+
+.globl display_total_and_array
+display_total_and_array:
+
+	jr $ra
+
