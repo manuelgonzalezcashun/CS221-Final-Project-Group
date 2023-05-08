23cc4a49ee4bc9d1cb0442ab57702582e5024ba0
Manuel Gonzalez Cashun <manuelgonzalezcashun@gmail.com>
2023-05-07 23:07:47 -0700

    Update main.asm

--- main.asm
+++ main.asm
@@ -25,9 +25,9 @@ error: .asciiz "Please input a valid number"
 
 .text 
 .globl main
-main:
+main: # Where everything is running
 	#print the introdcution and take the user_choice
-	li $v0, 4
+	li $v0, 4 # print string
 	la $a0, introduction_text
 	syscall 
 	main_2:
@@ -39,10 +39,10 @@ main:
 	move $t0, $v0 #move user_choice to $t0
 
 	#check beginning input of the user
-	beq $t0, 1, before_MAIN_LOOP
-	beq $t0, 2, EXIT_GAME
-	bgt $t0, 1, INVALID_NUMBER_p1
-	blt $t0, 2, INVALID_NUMBER_p1
+	beq $t0, 1, before_MAIN_LOOP # User wants to play game
+	beq $t0, 2, EXIT_GAME # User doesn't want to play game
+	bgt $t0, 1, INVALID_NUMBER_p1 #User entered invalid choice
+	blt $t0, 2, INVALID_NUMBER_p1 #User entered invalid choice
 
 
 	before_MAIN_LOOP:
@@ -57,65 +57,65 @@ main:
 			#create player hand, display the array, return the totals += to global variables
 				la $t1, player_card_read
 				la $t2, player_total_output
-				addiu $sp, $sp, -20
+				addiu $sp, $sp, -20 #allocates space on the stack
 				sw $s1, 0($sp)      #address of player array
 				sw $t1, 4($sp) 		#address of player_card_read
 				sw $t2, 8($sp)		#address of the player_total_output
 				sw $ra, 12($sp)		#save the $ra
-				jal update_hand
-				lw $t1, 16($sp)
-				lw $ra, 12($sp)
-				addiu $sp, $sp, 20
+				jal update_hand     #going to call update function
+				lw $t1, 16($sp)		#Load the return total
+				lw $ra, 12($sp)		#Load the return address
+				addiu $sp, $sp, 20  #Deallocates space on the stack
 				move $t5, $t1       #save the total to the global register
 				
 		part_2:
 			#create computer hand, display the array, return the totals += to global variables
 				la $t1, computer_card_read
 				la $t2, computer_total_output
-				addiu $sp, $sp, -20
+				addiu $sp, $sp, -20 #allocates space on the stack
 				sw $s2, 0($sp)      #address of computer array
 				sw $t1, 4($sp) 		#address of computer_card_read
 				sw $t2, 8($sp)		#address of the computer_total_output
 				sw $ra, 12($sp)		#save the $ra
-				jal update_hand
-				lw $t1, 16($sp)
-				lw $ra, 12($sp)
-				addiu $sp, $sp, 20
+				jal update_hand     #going to call update function
+				lw $t1, 16($sp)     #Load the return total
+				lw $ra, 12($sp)     #Load the return address
+				addiu $sp, $sp, 20  #Deallocates space on the stack
 				move $t6, $t1       #save the total to the global register
 
 
 		get_user_choice:
-			li $v0, 4
-			la $a0, continue_prompt
-			syscall
-			li $v0, 5
+			li $v0, 4 # print continue_propmt string
+			la $a0, continue_prompt # print the continue_prompt
+			syscall 
+			li $v0, 5 # Get User input fromt continue_prompt
 			syscall 
 
-			beq $v0, 1, user_choice_1
-			beq $v0, 2, user_choice_2
-			bgt $v0, 1, INVALID_NUMBER_p2
-			blt $v0, 2, INVALID_NUMBER_p2
+			beq $v0, 1, user_choice_1 # User wants to hit
+			beq $v0, 2, user_choice_2 # User wants to stand
+			bgt $v0, 1, INVALID_NUMBER_p2 #User entered invalid choice
+			blt $v0, 2, INVALID_NUMBER_p2 #User entered invalid choice
 			#ask the user (stand or hit?)
 			user_choice_1: #hit
 				la $t1, player_card_read
 				la $t2, player_total_output
-				addiu $sp, $sp, -20
+				addiu $sp, $sp, -20 #allocates space on the stack
 				sw $s1, 0($sp)      #address of player array
 				sw $t1, 4($sp) 		#address of player_card_read
 				sw $t2, 8($sp)		#address of the player_total_output
 				sw $ra, 12($sp)		#save the $ra
-				jal update_hand
-				lw $t1, 16($sp)
-				lw $ra, 12($sp)
-				addiu $sp, $sp, 20
+				jal update_hand		#going to call update function
+				lw $t1, 16($sp)  	#Load the return total
+				lw $ra, 12($sp)		#Load the return address
+				addiu $sp, $sp, 20	#Deallocates space on the stack
 				move $t5, $t1       #save the total to the global register
 
 				#JUST DISPLAY COMPUTER TOTAL
-				li $v0, 4
-				la $a0, computer_total_output
+				li $v0, 4 #print string
+				la $a0, computer_total_output #print the computer total points 
 				syscall
-				li $v0, 1
-				move $a0, $t6
+				li $v0, 1 #loading computer total into register
+				move $a0, $t6 #Saves computer total output to computer total global variable
 				syscall
 				
 
@@ -135,18 +135,18 @@ main:
 				sw $t1, 4($sp) 		#address of computer_card_read
 				sw $t2, 8($sp)		#address of the computer_total_output
 				sw $ra, 12($sp)		#save the $ra
-				jal update_hand
-				lw $t1, 16($sp)
-				lw $ra, 12($sp)
-				addiu $sp, $sp, 20
+				jal update_hand		#going to call update function
+				lw $t1, 16($sp)		#Load the return total
+				lw $ra, 12($sp)		#Load the return address
+				addiu $sp, $sp, 20  #Deallocates space on the stack
 				move $t6, $t1       #save the total to the global register
 
 				#JUST DISPLAY PLAYER TOTAL
-				li $v0, 4
-				la $a0, player_total_output
+				li $v0, 4 #print string
+				la $a0, player_total_output #print player_total_output
 				syscall
-				li $v0, 1
-				move $a0, $t5
+				li $v0, 1 #load player_total_output into register
+				move $a0, $t5 #Saves player total output to player total global variable
 				syscall
 				
 
@@ -158,43 +158,43 @@ main:
 
 			compare_computer_player:
 			#check if computer wins?
-			bgt $t6, $t5, computer_wins
-			bgt $t5, $t6, player_wins
+			bgt $t6, $t5, computer_wins #If computer is closer to 21 or is 21,  computer wins 
+			bgt $t5, $t6, player_wins #If player is closer to 21 or is 21, player wins
 
 			#TIE, by default
-			li $v0, 4
-			la $a0, tie_prompt
+			li $v0, 4 #print string
+			la $a0, tie_prompt #prints tie_prompt
 			syscall
-			b EXIT_GAME
+			b EXIT_GAME #Exits the game
 
 			player_wins:
-			li $v0, 4
-			la $a0, player_won
+			li $v0, 4 #print string
+			la $a0, player_won #prints player_won prompt
 			syscall
-			b EXIT_GAME
+			b EXIT_GAME #Exits the game
 
 			computer_wins:
-			li $v0, 4
-			la $a0, computer_won
+			li $v0, 4 #print string
+			la $a0, computer_won #prints computer_won prompt
 			syscall
-			b EXIT_GAME
+			b EXIT_GAME #Exits the game
 			
 	INVALID_NUMBER_p1:
-	li $v0, 4
-	la $a0, error
+	li $v0, 4 #print string
+	la $a0, error #prints error prompt to user
 	syscall
-	b main_2
+	b main_2 #returns to main_2
 	
 	INVALID_NUMBER_p2:
-	li $v0, 4
-	la $a0, error
+	li $v0, 4 #print string
+	la $a0, error #print error prompt to user
 	syscall
-	b get_user_choice
+	b get_user_choice #returns to get_user_choice
 
 		
 	EXIT_GAME:
-	li $v0, 4
-	la $a0, exit_output
+	li $v0, 4 #print string
+	la $a0, exit_output #print exit prompt to user
 	syscall
 	#TERMINATE THE PRORGAM
     li $v0, 10
